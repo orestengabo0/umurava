b3b3342aad7e32022fc4bfedc5e984793a56161b
orestengabo0 <orestengabo0@gmail.com>
2025-01-25 21:45:50 +0200

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/connectDB.js';
 
+dotenv.config();
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+connectDB();
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
