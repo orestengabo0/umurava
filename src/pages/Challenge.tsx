9ab334c200ffc08a8b88db3e6f1f03cf7002c7d5
benisamuel <benisamuel566@gmail.com>
2025-02-07 14:11:14 +0200

    Added First version community

--- src/pages/Challenge.tsx
+++ src/pages/Challenge.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Dashboard from '../layout/Dashboard'
+import React from "react";
+import Dashboard from "../layout/Dashboard";
+import TextCard from "../components/Home/TextCard/TextCard";
+import ChallengeWrapper from "../components/Home/ChallengeCards/ChallengeWrapper";
 
 const Challenge: React.FC = () => {
   return (
     <Dashboard>
       <div>
-        Hello Challenge
+        <div className=" flex flex-row items-center px-5 ">
+          <TextCard />
+        </div>
+        <div></div>
+        <div className=" flex flex-col px-5 gap-8 py-8">
+          <ChallengeWrapper />
+          <ChallengeWrapper />
+        </div>
       </div>
     </Dashboard>
-  )
-}
+  );
+};
 
-export default Challenge
+export default Challenge;
