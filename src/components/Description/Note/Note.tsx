53b940fa95e0a362169fcd3dab14b7c7c4b0f12d
benisamuel <benisamuel566@gmail.com>
2025-02-08 19:48:26 +0200

    Improved Notes

--- src/components/Description/Note/Note.tsx
+++ src/components/Description/Note/Note.tsx
@@ -5,7 +5,7 @@ const Note: React.FC = () => {
     <div className=" flex flex-col gap-4  px-8 ">
       <p className=" font-workSans font-semibold">Products Requirements</p>
       <ul className=" list-disc flex flex-col gap-2">
-        <li className=" font-workSans text-sm">Find Product Requirements Summary and Features Description for Saway Pay <span className=" underline">HERE</span></li>
+        <li className=" font-workSans text-sm">Find Product Requirements Summary and Features Description for Saway Pay <span className=" underline cursor-pointer ">HERE</span></li>
       </ul>
     </div>
   );
